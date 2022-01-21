7197dbdc44933eae5762ff41feba223e0f5e2ffe
Enigma Swan Adams <eswanadams@alaska.edu>
2022-01-20 21:38:56 -0900

    second part setup

---
+++ Project1/Source.cpp
@@ -0,0 +1,19 @@
+/*
+* file: Source.cpp
+* Enigma Swan Adams
+* 1/20/2022
+*
+* source code for the additional program 1 section of
+* homework 0
+*
+* prints one of two poems randomly
+*/
+
+#include <iostream>
+#include <ctime>
+using std::cout;
+
+int main() {
+	
+	return 0;
+}
