1c24186bf292e9b00aec1930668d1c422c2cfa82
Enigma Swan Adams <eswanadams@alaska.edu>
2022-01-20 18:57:10 -0900

    changed division in cpp_ctof and cpp_ftoc to be floating point

--- Program.cpp
+++ Program.cpp
@@ -23,7 +23,7 @@ using std::strtod;
  // converts const char pointer fahrenheit value to double celsius value
 double cpp_ftoc(const char* str){ 
 	double F = stod(str);
-	return (F - 32) * (5 / 9);
+	return (F - 32) * (5.0 / 9.0);
 }
 
 
@@ -31,7 +31,7 @@ double cpp_ftoc(const char* str){
 double cpp_ctof(const char* str) {
 	int* p = NULL;
 	double C = strtod(str, NULL);
-	return (9 / 5) * C + 32;
+	return (9.0 / 5.0) * C + 32;
 }
 
 
@@ -42,12 +42,12 @@ int main(int argc, const char** argv) {
 
 	if (*argv[1] == 'f' or *argv[1] == 'F') {
 		double C = cpp_ftoc(argv[2]);
-		cout << argv[2] << " in celcius is " << C << endl;
+		cout << argv[2] << " fahreheit, in celcius is " << C << endl;
 	}
 
 	else if (*argv[1] == 'c' or *argv[1] == 'C') {
 		double C = cpp_ctof(argv[2]);
-		cout << argv[2] << " in fahrenheit is " << C << endl;
+		cout << argv[2] << " celcius, in fahrenheit is " << C << endl;
 	}
 
 	else cout << "Incorect parameter given. Please use f or c to speficy the units of the starting tempertatur" << endl;
